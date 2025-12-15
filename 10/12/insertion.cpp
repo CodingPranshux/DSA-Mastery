01ba6787a87956f58ad64cfcf5d91a2add541513
CodingPranshux <pranshusingh910@gmail.com>
2025-12-15 23:29:22 +0530

    i

---
+++ 10/12/insertion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    // START AT i = 1 because element at index 0 is considered "sorted"
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
