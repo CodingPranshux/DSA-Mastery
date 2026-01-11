145b5653b26553c9b27b2213fb33bf50d3d2ff7c
CodingPranshux <pranshusingh910@gmail.com>
2026-01-11 23:47:47 +0530

    LCM base question

---
+++ B_Beautiful_Array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int tc;
+    cin>>tc;
+    while(tc--){
+        int n,k,b,s;
+        cin>>n>>k>>b,s;
+        
+
+       if((s>=b*k) && (s<(k-1)*n)){
+        for(int i=0;i<n;i++){
+            cout<<
+        }
+
+       }
+
+       
+
+    }
+} 
